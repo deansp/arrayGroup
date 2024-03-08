615907f2fc2f5c1ca68756989f5b91b9e8da9dbb
deanspecht <deansp@web.de>
2024-03-08 11:34:54 +0100

    step 2

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,11 @@ public class Main {
         int[] num = {1,2,3,4,5,6,7,8,9,10};
 
 
-
+        int sum = 0;
         for (int i = 0; i < num.length; i++) {
-            System.out.println(num[i]);
+            sum = sum + num[i];
         }
+
+        System.out.println(sum);
     }
 }
