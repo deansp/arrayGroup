4efd68a3188183c0e7fd7e73717e3a5f9f61b7f5
deanspecht <deansp@web.de>
2024-03-08 11:41:26 +0100

    step 3,4

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,15 @@ public class Main {
     public static void main(String[] args) {
 
         int[] num = {1,2,3,4,5,6,7,8,9,10};
+        int[] num2 = {11,12,130,14,15,16,17,18,19,20};
 
+        int maxNumber = num2[0];
 
-        int sum = 0;
-        for (int i = 0; i < num.length; i++) {
-            sum = sum + num[i];
+        for (int i = 1; i < num2.length; i++) {
+            if (num2[i] > maxNumber) {
+                maxNumber = num2[i];
+            }
         }
-
-        System.out.println(sum);
+        System.out.println(maxNumber);
     }
 }
